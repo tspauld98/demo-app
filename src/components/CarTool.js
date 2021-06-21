e71504cc009bb0960a772a5b3798d8ead57c4663
Tim Spaulding <tim_spaulding@intuit.com>
2021-06-21 14:32:32 -0700

    Completed exercise 2

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,9 +1,41 @@
 export const CarTool = () => {
 
   return (
-    <header>
-      <h1>Car Tool</h1>
-    </header>
+    <>
+      <header>
+        <h1>Car Tool</h1>
+      </header>
+      <table>
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Color</th>
+          <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>Toyota</td>
+          <td>Avalon Limited Hybrid</td>
+          <td>2019</td>
+          <td>White</td>
+          <td>$45,000</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Porche</td>
+          <td>911 Spyder</td>
+          <td>2021</td>
+          <td>Navy</td>
+          <td>$150,000</td>
+        </tr>
+        </tbody>
+      </table>
+    </>
   );
 
 };
