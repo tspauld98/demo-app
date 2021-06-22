5ca3c6d0b236e8130fb042a5411986707c0ae5c6
Tim Spaulding <tim_spaulding@intuit.com>
2021-06-22 11:27:18 -0700

    Added car form to CarTool

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+
 export const CarTool = (props) => {
+
+  const [ carForm, setCarForm ] = useState({
+    carMake: '',
+    carModel: '',
+    carYear: '',
+    carColor: '',
+    carPrice: '',
+  });
+
+  const change = (e) => {
+    setCarForm({
+      ...carForm,
+      [ e.target.name ]: e.target.value,
+    });
+  };
+
+  console.log(carForm);
 
   const carItems = props.cars.map((car) => {
     return (
@@ -31,6 +50,29 @@ export const CarTool = (props) => {
         </thead>
         <tbody>{carItems}</tbody>
       </table>
+      <br/>
+      <form>
+        <div>
+          <label htmlFor="car-make-input">Car Make</label>
+          <input type="text" id="car-make-input" value={carForm.carMake} onChange={change} name="carMake" />
+        </div>
+        <div>
+          <label htmlFor="car-model-input">Car Model</label>
+          <input type="text" id="car-model-input" value={carForm.carModel} onChange={change} name="carModel" />
+        </div>
+        <div>
+          <label htmlFor="car-year-input">Car Year</label>
+          <input type="text" id="car-year-input" value={carForm.carYear} onChange={change} name="carYear" />
+        </div>
+        <div>
+          <label htmlFor="car-color-input">Car Color</label>
+          <input type="text" id="car-color-input" value={carForm.carColor} onChange={change} name="carColor" />
+        </div>
+        <div>
+          <label htmlFor="car-price-input">Car Price</label>
+          <input type="text" id="car-price-input" value={carForm.carPrice} onChange={change} name="carPrice" />
+        </div>
+      </form>
     </>
   );
 
