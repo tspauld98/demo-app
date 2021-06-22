5724b9a32891304501d62ca37187ebb5c7bdc3c1
Tim Spaulding <tim_spaulding@intuit.com>
2021-06-22 11:03:29 -0700

    Created a form and a model

--- src/components/ColorTool.js
+++ src/components/ColorTool.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 export const ColorTool = ({ colors }) => {
+
+  const [ colorForm, setColorForm ] = useState({
+    colorName: '',
+    colorHexcode: '',
+  });
+
+  const change = (e) => {
+    setColorForm({
+      ...colorForm,
+      [ e.target.name ]: e.target.value,
+    });
+  };
+
+  console.log(colorForm);
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>{colors.map(color => <li key={color.id}>{color.name}</li>)}</ul>
+      <form>
+        <div>
+          <label htmlFor="color-name-input">Color Name</label>
+          <input type="text" id="color-name-input" value={colorForm.colorName} onChange={change} name="colorName" />
+        </div>
+        <div>
+          <label htmlFor="color-hexcode-input">Color Hexcode</label>
+          <input type="text" id="color-hexcode-input" value={colorForm.colorHexcode} onChange={change} name="colorHexcode" />
+        </div>
+      </form>
     </>
   );
 };
